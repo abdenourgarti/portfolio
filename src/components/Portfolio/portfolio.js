0db2c9b3cedac25943df6aab5988fed5e631f829
Abdenour Garti <garti.abdenour@gmail.com>
2024-09-18 15:51:10 +0100

    portfolio to projects

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -49,7 +49,7 @@ const Project = ({lien, image, alt}) => {
 const Portfolio = () => {
   return (
     <section id="portfolio">            
-        <h2>Portfolio</h2>
+        <h2>Projects</h2>
         <div className="projets">
             {projects.map((projet, index) => (
                 <Project key={index} lien={projet.lien} image={projet.image} alt={projet.alt}/>
